ac15d4ede70a9e723021793a202fa6f3179a82b0
Talita Sabine <tainakasai01@gmail.com>
2025-04-09 11:05:18 -0300

    resolução das questões

--- Aula-05-atividade/src/CorpoHumano.java
+++ Aula-05-atividade/src/CorpoHumano.java
@@ -7,4 +7,40 @@ public class CorpoHumano {
 
     // ou definir dados
     // private float massa, volume, densidade;
+
+    // o construtor é utilizado para atribuir os valores para todos os atributos de uma vez, sem precisar o setter, de variável por variável
+    public CorpoHumano(float massa, float volume) {
+        this.massa = massa;
+        this.volume = volume;
+        this.densidade = massa/volume;
+        // a densidadee seá alterada assim que tiver a massa e o volume
+    }
+
+    //Setters: atribuir valores aos atributos
+    // void não é obrigatório o return
+    public void setMassa(float massa) {
+        this.massa = massa;
+        this.densidade = massa/volume;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+        this.densidade = massa/volume;
+    }
+
+    // Getters: recuperar valores dos atributos
+    // return se torna obrigatório a metodos que possuem um tipo de dado
+    public float getMassa() {
+        return this.massa;
+    }
+
+    public float getVolume() {
+        return this.volume;
+    }
+
+    public float getDensidade() {
+        return this.densidade;
+    }
+
+
 }
--- Aula-05-atividade/src/Main.java
+++ Aula-05-atividade/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // instanciando a classe ou criar o objeto
+        CorpoHumano obj = new CorpoHumano(0,0);
+
+        obj.setMassa(10);
+        obj.setVolume(2);
+        // Qual a ocorrência?
+        // R.: Deu erro no código
+        // Qual a conclusão sobre o que aconteceu?
+        // R.: O metodo ficou privado e nisso é invisivel (ou oculto)
+
+        // obj.massa = 10;
+            // Qual a ocorrência?
+            // R.: Deu erro no código
+            // Qual a conclusão sobre o que aconteceu?
+            // R.: O atributo está sendo acessado de forma direta, mas está como private
+        System.out.println(obj.getDensidade());
+        System.out.println(obj.getMassa());
+        // obj.massa = 20;
+            // Qual a ocorrência?
+            // R.: Não deu erro no código
+            // qual a conclusão sobre o que aconteceu?
+            // R.: O atributo em público é acessível para todos
+
     }
 }
