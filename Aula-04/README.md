545b866ee3a33e2e515daf9eb58571682e0dc2d0
Talita Sabine <tainakasai01@gmail.com>
2025-04-09 10:21:12 -0300

    update pastas

--- Aula-04/README.md
+++
@@ -1,2 +0,0 @@
-# Poo-Aula-04
-Aula sobre versionamento de código em Programação Orientada a Objeto - introdução a Git e Github.
---
+++ Aula-05-atividade/src/CorpoHumano.java
@@ -0,0 +1,10 @@
+public class CorpoHumano {
+    // definindo variaveis
+
+    private float massa;
+    private float volume;
+    private float densidade;
+
+    // ou definir dados
+    // private float massa, volume, densidade;
+}
