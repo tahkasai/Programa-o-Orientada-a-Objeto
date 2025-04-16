2ed9334dbd7134dc571bf0da6f9fa5147a0bfec7
Gabriel Rodrigues <136513139+tahkasai@users.noreply.github.com>
2025-04-16 10:12:03 -0300

    Update Main.java

--- Aula-07/src/Main.java
+++ Aula-07/src/Main.java
@@ -1,27 +1,35 @@
 public class Main {
-    public static void main(String[] args) {
-        // conversão de variáveis
-
+    public static void main(String[] args) throws Exception {
         String st, st2;
         float real, real2;
-        int n1, n2;
+        int  n1, n2;
 
-        // String para real
-        st = "12";
-        real = (float) (new Float(st).floatValue());
-        System.out.println("String para float: "+real);
+        //String para Double
 
-        // Double para String
-        real2 = 12;
+        st="12";
+        st="12"+"10";
+        real= (float) (new Float(st).floatValue());
+        real+=10;
+        System.out.println("String para Float: "+real);
+
+
+        //Double para String
+        real2=10;
+        st2= ""+real; //ou
         st2 = new Float(real2).toString();
+        System.out.println("Double para String: "+st2);
 
-        // String para Int
-        st2 = "10";
+        //String para Int
+        st2="10";
         n1= Integer.parseInt(st2);
+        System.out.println("String para int: "+n1);
 
-        // Int pra String
+        //Int para String
         n2=10;
-        st2=Integer.toString(n2);
+        st2= Integer.toString(n2);//ou
+        st2 = ""+n2;
+        System.out.println("Int para String: "+st2);
+
 
     }
 }
