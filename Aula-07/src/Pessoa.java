b709ae74578c4810139d65d55fed4a2fb01acdc5
Gabriel Rodrigues <136513139+tahkasai@users.noreply.github.com>
2025-04-16 10:40:01 -0300

    Create Pessoa.java

---
+++ Aula-07/src/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+    private final String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }	
+
+    public String getNome() {
+        return nome;
+    }
+
+}
