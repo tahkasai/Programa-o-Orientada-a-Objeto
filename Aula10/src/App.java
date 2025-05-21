eec40ebda7ea245975ea3cffc1d614c4b2d884a6
Gabriel Rodrigues <136513139+tahkasai@users.noreply.github.com>
2025-05-21 10:29:22 -0300

    update App.java

--- Aula10/src/App.java
+++ Aula10/src/App.java
@@ -3,5 +3,13 @@ public class App {
         Professor p1 = new Professor("Tainá Kasai",123);
         Professor p2 = new Professor("Gabrilele Monteiro",231);
         Professor p3 = new Professor("Talita Sabine",321);
+
+        Departamento colegiado = new Departamento("ads", 1);
+
+        colegiado.adicionarProfessor(p1);
+        colegiado.adicionarProfessor(p2);
+        colegiado.adicionarProfessor(p3);
+
+        colegiado.listarProfessor();
     }
 }
